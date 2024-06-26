fe5d99ba74e10ed3fc5c1dcd8928ba459af9764b
Strangeyboy <info@jamesstrange.co.uk>
2024-06-27 00:10:56 +0100

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
     './**/*.css'
   ],
+  safelist: [
+    'col-span-1',
+    'col-span-2',
+    'col-span-3',
+    'col-span-4',
+    'col-span-5',
+    'col-span-6',
+    'col-span-7',
+    'col-span-8',
+    'col-span-9',
+    'col-span-10',
+    'col-span-11',
+    'col-span-12',
+    'col-start-9',
+    'md:col-span-6',
+    'md:col-start-9',
+    'md:col-span-2'
+  ],
   theme: {
     extend: {
       spacing: {
